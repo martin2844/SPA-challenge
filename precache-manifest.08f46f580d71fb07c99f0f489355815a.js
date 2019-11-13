f68370c26e2fe501e8e0f073840951ff299e0362
unknown <martin2844@gmail.com>
2019-11-13 14:00:20 -0300

    Updates

---
+++ precache-manifest.08f46f580d71fb07c99f0f489355815a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a7567e70b7415491ecc7340602fc42",
+    "url": "/SPA-challenge/index.html"
+  },
+  {
+    "revision": "b6d38fcd73fd9a9f94f5",
+    "url": "/SPA-challenge/static/css/main.54e6a6a9.chunk.css"
+  },
+  {
+    "revision": "a45b8d3e7b0cfd282b6c",
+    "url": "/SPA-challenge/static/js/2.43b890ac.chunk.js"
+  },
+  {
+    "revision": "b6d38fcd73fd9a9f94f5",
+    "url": "/SPA-challenge/static/js/main.8c814b8d.chunk.js"
+  },
+  {
+    "revision": "2b510a685656161d0285",
+    "url": "/SPA-challenge/static/js/runtime-main.913f48f2.js"
+  }
+]);
